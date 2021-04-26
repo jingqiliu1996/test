d34be79d728233c9fb4db1ca71eeddf8259ba6b7
Jingqi Liu <jliu0338@uni.sydney.edu.au>
2021-04-26 10:00:57 +0800

    campusfloraweb

---
+++ campusfloraweb/app/admin/trail.rb
@@ -0,0 +1,49 @@
+ActiveAdmin.register Trail do
+  permit_params :name, :slug, :information, species_location_trails_attributes: [:id, :species_location_id, :trail_id, :_destroy]
+  remove_filter :species_location_trails
+
+  controller do
+    def find_resource
+      scoped_collection.where(slug: params[:id]).first!
+    end
+  end
+
+  form do |f|
+    f.actions # adds the 'Submit' and 'Cancel' buttons
+    f.semantic_errors # shows errors on :base
+    f.inputs 'Details' do
+      f.inputs          # builds an input field for every attribute
+    end
+
+    f.inputs 'Individual Plants' do
+      f.has_many :species_location_trails, heading: "Individual Plants", allow_destroy: true, new_record: true do |a|
+        a.input :species_location_id, :label => 'Plant (Species Location) ID', :as => :select, :collection => SpeciesLocation.all.order(:arborplan_id).map{|s| ["ID: #{s.id} - Arborplan ID: #{s.arborplan_id} - #{s.species && s.species.genusSpecies} (#{s.lat}, #{s.lon})", s.id]}
+      end
+    end
+
+    # f.inputs 'Select Species' do
+    #   f.input :species, :collection => Species.pluck(:genusSpecies, :id)
+    # end
+
+    f.actions # adds the 'Submit' and 'Cancel' buttons
+  end
+
+  show do |trail|
+    attributes_table do
+      row :name
+      row :slug
+      row :information
+    end
+
+    panel 'Individual Plants' do
+      table_for trail.species_locations do
+        column 'ID', :id
+        column 'Arborplan ID', :arborplan_id
+        column 'Latitude', :lat
+        column 'Longitude', :lon
+      end
+    end
+
+    active_admin_comments_for(resource)
+  end
+end
---
+++ campusfloraweb/app/models/trail.rb
@@ -0,0 +1,23 @@
+class Trail < ActiveRecord::Base
+  extend FriendlyId
+  
+  has_many :species_location_trails
+  has_many :species_locations, through: :species_location_trails, :class_name => 'SpeciesLocation'
+
+  friendly_id :name, use: :slugged
+
+  accepts_nested_attributes_for :species_locations
+  accepts_nested_attributes_for :species_location_trails, :allow_destroy => true
+
+  after_save :expire_cache
+
+  def expire_cache
+    Rails.cache.clear
+  end
+
+  def slug=(value)
+    if value.present?
+      write_attribute(:slug, value)
+    end
+  end
+end
